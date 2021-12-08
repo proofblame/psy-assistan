13a16f2249c52ad9026538e4f3b1cc03ded1a936
Timur Marokko <proofblame@gmail.com>
2021-12-08 13:06:27 +0300

    root

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import style from './App.module.scss'
+function App() {
+  return (
+    <div className={style.app}>
+      <h1 className={style.app__title}>Привет</h1>
+      {/* <p className="app__subtitle">Подзаголовок</p>
+      <a href="#" className="app__link">ссылка</a> */}
+    </div>
+  );
+}
+
+export default App;
