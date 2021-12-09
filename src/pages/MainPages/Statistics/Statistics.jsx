1e573195226097a40e4fdec7d7729d24a463111e
Timur Marokko <proofblame@gmail.com>
2021-12-09 18:27:50 +0300

    ref

--- src/pages/MainPages/Statistics/Statistics.jsx
+++ src/pages/MainPages/Statistics/Statistics.jsx
@@ -1,6 +1,7 @@
 import statistics from './Statistics.module.scss'
 import Container from '../../../UI/Container/Container'
 import IMG from '../../../UI/IMG/IMG'
+import Button from '../../../UI/Button/Button'
 import img from './images/statistics.svg'
 
 const Statistics = () => {
@@ -10,10 +11,13 @@ const Statistics = () => {
         <div className={statistics.wrapper}>
           <IMG
             src={img} alt="Статистика результатов"
+            className={statistics.img}
           />
           <div className={statistics.subwrapper}>
-
+            <p className={statistics.title}>81%</p>
+            <p className={statistics.text}>наших клиентов чувствуют результат уже после пятой сессии</p>
           </div>
+          <Button className='dark'>Подобрать психолога сейчас</Button>
         </div>
       </Container>
     </section>
