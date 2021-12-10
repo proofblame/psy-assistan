8cdf6c9859b15a7d4ad9a4aa4402223807de1b3a
Timur Marokko <proofblame@gmail.com>
2021-12-10 22:28:21 +0300

    fix

--- src/pages/Main/ChooseSpec/ChooseSpec.jsx
+++ src/pages/Main/ChooseSpec/ChooseSpec.jsx
@@ -4,35 +4,50 @@ import Wrap from '../../../UI/Wrap/Wrap';
 import H1 from '../../../UI/H1/H1';
 import H3 from '../../../UI/H3/H3';
 import Button from '../../../UI/Button/Button';
+import Container from '../../../UI/Container/Container';
 
 const ChooseSpec = () => {
   return (
     <section className={choose.spec}>
-      <div className={choose.background}>
-        <Wrap className={choose.wrap}>
-          <div className={choose.headerWrap}>
-            <H1 className={choose.headerMain}>
-              Общайтесь <br /> с психологом в любое время и в любом месте
-            </H1>
-            <H3 className={choose.subheader}>
-              Онлайн консультации с проверенными специалистами
-            </H3>
+      <div className={choose.specContainer}>
+        <Container>
+          <div className={choose.container}>
+            <Wrap className={choose.wrap}>
+              <div className={choose.headerWrap}>
+                <H1 className={choose.headerMain}>
+                  Общайтесь <br /> с психологом в любое время и в любом месте
+                </H1>
+                <h3 className={choose.subheader}>
+                  Онлайн консультации с проверенными специалистами
+                </h3>
+                <div className={choose.buttonWrapper}>
+                  <Button className='dark'>Выбрать специалиста</Button>
+                  <p className={choose.text}>2 850 ₽ за сессию</p>
+                </div>
+              </div>
+            </Wrap>
             <div className={choose.buttonContainer}>
-              <Button className='dark'>Выбрать специалиста</Button>
-              <p className={choose.text}>2 850 ₽ за сессию</p>
+              <div>
+                <p className={choose.info}>
+                  100%
+                </p>
+                <p className={choose.infoSub}>
+                  конфиденциальность
+                </p>
+              </div>
+              <div>
+                <p className={choose.info}>
+                  1 500
+                </p>
+                <p className={choose.infoSub}>
+                  специалистов
+                </p>
+              </div>
             </div>
           </div>
-        </Wrap>
+        </Container>
       </div>
-      <div className={choose.backgroundLine}></div>
-      <div className={choose.buttonContainer}>
-        <p className={choose.info}>
-          100% <span className={choose.infoSub}> конфиденциальность</span>
-        </p>
-        <p className={choose.info}>
-          1 500 <span className={choose.infoSub}>специалистов</span>
-        </p>
-      </div>
+
     </section>
   );
 };
--- src/pages/Main/Specialists/Specialists.jsx
+++ src/pages/Main/Specialists/Specialists.jsx
@@ -6,62 +6,62 @@ import UL from '../../../UI/UL/UL';
 import Specialist from './images/Specialist.svg';
 import Li from '../../../UI/UL/Li/Li';
 import Button from '../../../UI/Button/Button';
-import Container from '../../../UI/Container/Container';
+import Container from '../../../UI/Container/Container'
 import Wrap from '../../../UI/Wrap/Wrap';
 
 const Specialists = () => {
   return (
     <section className={spec.specialists}>
-      <Wrap>
-        <div className={spec.headerWrapper}>
+      <Container>
+        <div className={spec.container}>
           <H2 className={spec.header}>
-            Мы отбираем только опытных специалистов
+            Мы отбираем только опытных специалистов <br /><br />
+            <span>Вы можете нам доверять!</span>
           </H2>
-          <H3 className={spec.subheader}>Вы можете нам доверять!</H3>
-        </div>
-        <div className={spec.wrapper}>
-          <div className={spec.imgWrapper}>
-            <p className={spec.text}>+ 1500 специалистов в вашем телефоне</p>
-            <IMG
-              src={Specialist}
-              alt='специалисты'
-              className={spec.specialist}
-            ></IMG>
+          <div className={spec.wrapper}>
+            <figure className={spec.imgWrapper}>
+              <p className={spec.text}>+ 1500 специалистов в вашем телефоне</p>
+              <IMG
+                src={Specialist}
+                alt='специалисты'
+                className={spec.img}
+              ></IMG>
+            </figure>
+            <UL className={spec.list}>
+              <Li className={spec.listItemEdu}>
+                <H3 className={spec.listHeader}>Образование</H3>
+                <p className={spec.listText}>
+                  Высшее психологическое, гуманитарное и дополнительное
+                  психотерапевтическое.
+                </p>
+              </Li>
+              <Li className={spec.listItemExp}>
+                <H3 className={spec.listHeader}>Опыт работы</H3>
+                <p className={spec.listText}>
+                  От трех лет. За это время накапливается хороший практический
+                  опыт
+                </p>
+              </Li>
+              <Li className={spec.listItemInter}>
+                <H3 className={spec.listHeader}>Собеседование</H3>
+                <p className={spec.listText}>
+                  Проверяем самое важное: навыки и успешные кейсы из практики.
+                </p>
+              </Li>
+              <Li className={spec.listItemStudy}>
+                <H3 className={spec.listHeader}>Обучение</H3>
+                <p className={spec.listText}>
+                  И мастерам важно учиться. Развиваем через семинары и супервизии.
+                </p>
+              </Li>
+            </UL>
           </div>
-          <UL className={spec.list}>
-            <Li className={spec.listItemEdu}>
-              <H3 className={spec.listHeader}>Образование</H3>
-              <p className={spec.listText}>
-                Высшее психологическое, гуманитарное и дополнительное
-                психотерапевтическое.
-              </p>
-            </Li>
-            <Li className={spec.listItemExp}>
-              <H3 className={spec.listHeader}>Опыт работы</H3>
-              <p className={spec.listText}>
-                От трех лет. За это время накапливается хороший практический
-                опыт
-              </p>
-            </Li>
-            <Li className={spec.listItemInter}>
-              <H3 className={spec.listHeader}>Собеседование</H3>
-              <p className={spec.listText}>
-                Проверяем самое важное: навыки и успешные кейсы из практики.
-              </p>
-            </Li>
-            <Li className={spec.listItemStudy}>
-              <H3 className={spec.listHeader}>Обучение</H3>
-              <p className={spec.listText}>
-                И мастерам важно учиться. Развиваем через семинары и супервизии.
-              </p>
-            </Li>
-          </UL>
+          <div className={spec.buttonWrapper}>
+            <Button className='dark'>Подобрать своего специалиста</Button>
+          </div>
+          <p className={spec.confidential}>100% конфиденциальность</p>
         </div>
-        <div className={spec.buttonWrapper}>
-          <Button className='dark'>Подобрать своего специалиста</Button>
-        </div>
-        <p className={spec.confidential}>100% конфиденциальность</p>
-      </Wrap>
+      </Container>
     </section>
   );
 };
