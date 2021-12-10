97f7671c7e119711b7e37901742c873c23e22271
Timur Marokko <proofblame@gmail.com>
2021-12-11 01:12:27 +0300

    landing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,99 +6,111 @@ import IMG from '../../UI/IMG/IMG';
 import Facebook from '../Footer/images/Facebook.svg';
 import Telegram from '../Footer/images/Telegram.svg';
 import Whatsup from '../Footer/images/Whatsup.svg';
+import Container from '../../UI/Container/Container';
+import Button from '../../UI/Button/Button';
 
 const Footer = () => {
   return (
     <section className={footer.footer}>
-      <div className={footer.container}>
-        <div className={footer.socialContainer}>
-          <H3 className={footer.footerHeader}>
-            Если есть вопросы - пишите нам{' '}
-          </H3>
-          <p className={footer.subheader}>Мы будем рады ответить на них!</p>
-          <nav className={footer.social}>
-            <UL className={footer.socialNavList}>
-              <Li className={footer.socialNavItem}>
-                <a href='https://www.facebook.com/'>
-                  <IMG
-                    src={Facebook}
-                    alt={'facebook'}
-                    className={footer.image}
-                  ></IMG>
-                </a>
-              </Li>
-              <Li className={footer.socialNavItem}>
-                <a href='https://web.telegram.org/'>
-                  <IMG
-                    src={Telegram}
-                    alt={'telegram'}
-                    className={footer.image}
-                  ></IMG>
-                </a>
-              </Li>
-              <Li className={footer.socialNavItem}>
-                <a href='https://www.whatsapp.com/'>
-                  <IMG
-                    src={Whatsup}
-                    alt={"whats'up"}
-                    className={footer.image}
-                  ></IMG>
-                </a>
-              </Li>
-            </UL>
-          </nav>
-        </div>
-        <nav className={footer.footerList}>
-          <UL>
-            <Li className={footer.navItem}>
-              <a href='#' className={footer.navItemLink}>
-                Вопросы-ответы
-              </a>
-            </Li>
-            <Li className={footer.navItem}>
-              <a href='#' className={footer.navItemLink}>
-                Контакты
-              </a>
-            </Li>
-            <Li className={footer.navItem}>
-              <a href='#' className={footer.navItemLink}>
-                Работа в Psy-assistant
-              </a>
-            </Li>
-            <Li className={footer.navItem}>
-              <a href='#' className={footer.navItemLink}>
-                Блог
-              </a>
-            </Li>
-            <Li className={footer.navItem}>
-              <a href='#' className={footer.navItemLink}>
-                Psy-assistant для бизнеса
-              </a>
-            </Li>
-            <Li className={footer.navItem}>
-              <a href='#' className={footer.navItemLink}>
-                Проверка совместимости
-              </a>
-            </Li>
-          </UL>
-        </nav>
-        <div className={footer.feedback}>
-          <p className={footer.text}>
-            Регистрация на сайте означает согласие{' '}
-            <a href='#' className={footer.textLink}>
-              с пользовательским соглашением.
-            </a>{' '}
-            Политика в отношении обработки и защиты персональных данных.
-          </p>
-          <div className={footer.like}>
-            <div className={footer.likeContainer}>
-              <p className={footer.likeText}>Нравится!</p>
-              <p className={footer.likeCounter}>35 тыс.</p>
-            </div>
-            <button className={footer.likeButton}>Поделиться</button>
+      <Container>
+        <div className={footer.container}>
+
+          <div className={footer.socialContainer}>
+            <H3 className={footer.footerHeader}>
+              Если есть вопросы - пишите нам
+            </H3>
+            <p className={footer.subheader}>Мы будем рады ответить на них!</p>
+            <nav className={footer.social}>
+              <ul className={footer.socialNavList}>
+                <Li className={footer.socialNavItem}>
+                  <a href='https://www.facebook.com/'>
+                    <IMG
+                      src={Facebook}
+                      alt={'facebook'}
+                      className={footer.image}
+                    ></IMG>
+                  </a>
+                </Li>
+                <Li className={footer.socialNavItem}>
+                  <a href='https://web.telegram.org/'>
+                    <IMG
+                      src={Telegram}
+                      alt={'telegram'}
+                      className={footer.image}
+                    ></IMG>
+                  </a>
+                </Li>
+                <Li className={footer.socialNavItem}>
+                  <a href='https://www.whatsapp.com/'>
+                    <IMG
+                      src={Whatsup}
+                      alt={"whats'up"}
+                      className={footer.image}
+                    ></IMG>
+                  </a>
+                </Li>
+              </ul>
+            </nav>
           </div>
+
+          <nav className={footer.footerList}>
+            <ul className={footer.centerList}>
+              <Li className={footer.navItem}>
+                <a href='#' className={footer.navItemLink}>
+                  Вопросы-ответы
+                </a>
+              </Li>
+              <Li className={footer.navItem}>
+                <a href='#' className={footer.navItemLink}>
+                  Контакты
+                </a>
+              </Li>
+              <Li className={footer.navItem}>
+                <a href='#' className={footer.navItemLink}>
+                  Работа в Psy-assistant
+                </a>
+              </Li>
+              <Li className={footer.navItem}>
+                <a href='#' className={footer.navItemLink}>
+                  Блог
+                </a>
+              </Li>
+              <Li className={footer.navItem}>
+                <a href='#' className={footer.navItemLink}>
+                  Psy-assistant для бизнеса
+                </a>
+              </Li>
+              <Li className={footer.navItem}>
+                <a href='#' className={footer.navItemLink}>
+                  Проверка совместимости
+                </a>
+              </Li>
+            </ul>
+          </nav>
+
+          <div className={footer.feedback}>
+            <p className={footer.text}>
+              Регистрация на сайте означает согласие
+              <a href='#' className={footer.textLink}>
+                с пользовательским соглашением.
+              </a> <br />
+              Политика в отношении обработки и защиты персональных данных.
+            </p>
+            <div className={footer.like}>
+              <div className={footer.likeWrapper}>
+                <div className={footer.likeContainer}>
+                  <p className={footer.likeText}>Нравится!</p>
+                  <p className={footer.likeCounter}>35 тыс.</p>
+                </div>
+                <div className={footer.likeButton}>
+                  <Button className='dark'>Поделиться</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
+      </Container>
     </section>
   );
 };
